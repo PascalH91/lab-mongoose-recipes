f42cb7f4e61ca29eb0953109e254b74563fd01bb
Pascal Hanke <pascal.hanke@yahoo.de>
2019-11-08 11:45:44 +0100

    done

--- index.js
+++ index.js
@@ -1,12 +1,70 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+// Recipe - empty documentcreation
+// Recipe.create({
+//   cuisine: "German"
+// })
+//   .then(document => {
+//     console.log(document);
+//   })
+//   .catch(err => {
+//     console.log("ERROR: ", err);
+//   });
+
+// Recipe.insertMany(data)
+//   .then(document => {
+//     console.log(document);
+//   })
+//   .catch(err => {
+//     console.log("ERROR: ", err);
+//   });
+
+// Recipe.findByIdAndDelete("5dc53c308a910016b11d4ccd").then(document => {
+//   console.log(document);
+// });
+/*   {_id: "5dc53c308a910016b11d4cce"},
+  {_id: "5dc53c308a910016b11d4ccf"},
+  {_id: "5dc53c308a910016b11d4cd0"},
+  {_id: "5dc53c308a910016b11d4cd1"} */
+// Recipe.deleteMany({
+//   _id: "5dc53c308a910016b11d4cce",
+// }).then(document => {
+//   console.log(document);
+// });
+// Recipe.find().then(document => {
+//   document.forEach(el => {
+//     console.log(el.title);
+//   });
+// });
+
+// Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+//   .then(response => {
+//     console.log("succesfull UPDATE!");
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+/* Recipe.deleteOne({ title: "Carrot Cake" }).then(() => {
+  console.log("deleted Carrotcake!!");
+}); */
+
+mongoose.connection.close();
+// .then(() => {
+//   console.log("Connected CLOSED!");
+// })
+// .catch(err => {
+//   console.error("Error connecting to mongo", err);
+// });;
